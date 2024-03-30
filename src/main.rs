3f8736ee2dcef3bb8d3d5aed5bb238d8d208b034
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2024-03-30 14:12:41 -0400

    perf: skip evens

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod toitent;
 use primes::Sieve;
 use toitent::totient;
 
+/// Check if a number is a [Perfect Totient Number](https://en.wikipedia.org/wiki/Perfect_totient_number).
 fn is_perfect_toitent(n: u64, sieve: &mut Sieve) -> bool {
     let mut t = n;
     let mut sum: u64 = 0;
@@ -23,8 +24,7 @@ fn main() {
     for i in 0..max_threads {
         let handle = std::thread::spawn(move || {
             let mut sieve = Sieve::new();
-            for n in (2..u64::MAX).skip(i).step_by(max_threads) {
-                // println!("Thread {}: {}", i, n);
+            for n in (3..u64::MAX).skip(i).step_by(max_threads * 2) {
                 if is_perfect_toitent(n, &mut sieve) {
                     println!("{}", n);
                 }
